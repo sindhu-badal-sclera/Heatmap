09c84ec42863b354544b2619a097e893761dda33
Sindhu <sindhu.badal@sclera.com>
2024-12-11 11:59:06 +0530

    changes in navbar texts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Heatmap from "./heatmap";
 import Cctv from "./cctv";
 
+
+
 function App() {
+const[clicked, setclicked]=useState(false)
+function handleClick(){
+  setclicked((prevState)=> !prevState)
+}
+
   return (
     <Router>
       <div style={{width:'100vw', height:'100vh'}} >
@@ -17,12 +24,17 @@ function App() {
             justifyContent: "space-around",
           }}
         >
-          <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+         <div style={{backgroundColor: clicked? "inherit" : "grey"}} onClick={handleClick}>
+         <Link to="/" style={{ color: "white", textDecoration: "none" }} > 
             WiFi Heatmaps
           </Link>
-          <Link to="/cctv" style={{ color: "white", textDecoration: "none" }}>
+          </div>
+          <div style={{backgroundColor: clicked? "grey" : "inherit"}} onClick={handleClick}>
+          <Link to="/cctv" style={{ color: "whited", textDecoration: "none"}}>
             CCTV Cameras
           </Link>
+         
+         </div>
         </nav>
 
         {/* Define Routes */}
